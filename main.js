2b3f608c1fc6a78f272c441262b962aff940d684
apmorris83 <apmorris83@gmail.com>
2017-01-25 11:34:59 +0000

    jquery working for when player select correct answer

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('hello');
+//console.log('hello');
+
+/*guess the correct answer*/
+
+$(document).ready(function(){
+  $('.correct').click(function(){
+    $(this).removeClass('guess');
+    $(this).addClass('chosen-correct')
+
+  })
+});
+
+/*guess the incorrect answer*/
